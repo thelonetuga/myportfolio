013c8857229c489eda526626359ab204d07717f5
João Marques <joao.marques.code@gmail.com>
2022-03-28 22:16:37 +0100

    sigurate

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -14,7 +14,7 @@ function Hero() {
         </div>
         <div class="lg:flex-grow md:w-1/2 lg:pl-24 md:pl-16 flex flex-col md:items-start md:text-left items-center text-center">
           <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-indigo-blue">
-            About me
+            About me..
           </h1>
           <p class="mb-8 leading-relaxed text-justify text-indigo-darkblue text-lg">
             My name is João Marques and I'm from Tomar, Portugal. Currently, I
